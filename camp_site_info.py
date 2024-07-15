3c754b1de61adbd52a48c531bf93d570600deb84
redranges <redranges@protonmail.com>
2024-07-14 21:45:46 -0400

    Changes to camp_site_urls.py
    Script now will detect page end so that we can iterate through all pages
    dynamically as number of pages grows or shrinks
    
    Additional changes to where data files are stored
    
    Ability to create directory for given date and store data there. This
    can be useful to compare data over time.
    
    Formatting of code has also been improved

--- camp_site_info.py
+++ camp_site_info.py
@@ -5,6 +5,7 @@ import time
 from sys import argv
 import os
 
+
 def scrape_lat_long(html):
     soup = BeautifulSoup(html, 'html.parser')
     divs = soup.find_all('div', attrs={'class': 'right-box'})
@@ -19,22 +20,24 @@ def scrape_lat_long(html):
             break
     return (latitude, longitude)
 
+
 def fetch_camp_site_content(url):
     headers = {'User-Agent': 'Camping Tripper/0.0.1'}
     response = requests.get(url, headers=headers)
-    return response.content 
+    return response.content
+
 
 if __name__ == '__main__':
     try:
         batch_start = int(argv[1])
         batch_end = int(argv[2])
-    except IndexError as e:
+    except IndexError:
         raise SystemExit(f'Usage: {argv[0]} <batch_start> <batch_end>')
 
     CAMP_SITE_CSV_PATH = './camp_site_urls.csv'
     CAMP_SITE_INFO_CSV_PATH = './camp_site_info.csv'
     SLEEP_TIME = 5
-    
+
     df = pd.read_csv(CAMP_SITE_CSV_PATH)
     df = df.loc[batch_start:batch_end-1]
 
@@ -49,5 +52,7 @@ if __name__ == '__main__':
         time.sleep(SLEEP_TIME)
 
     write_header = False if os.path.exists(CAMP_SITE_INFO_CSV_PATH) else True
-    df.to_csv(CAMP_SITE_INFO_CSV_PATH, mode='a', index=False, header=write_header)
-
+    df.to_csv(CAMP_SITE_INFO_CSV_PATH,
+              mode='a',
+              index=False,
+              header=write_header)
--- camp_site_urls.py
+++ camp_site_urls.py
@@ -1,38 +1,63 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
 import requests
 import time
 import pandas as pd
+import os
+
+
+def create_date_str_path():
+    return time.strftime("%m_%d_%Y/")
+
+
+def scrape_ending_page():
+    url=BASE_URL+str(STARTING_PAGE)
+    attrs={'aria-label' : 'Last page'}
+    response=requests.get(url, headers=HEADERS)
+    soup=BeautifulSoup(response.content, 'html.parser')
+    ending_page_element=soup.find_all(attrs=attrs)
+    ending_page_url=ending_page_element[0]['href']
+    parsed_ending_page_url=urlparse(ending_page_url)
+    ending_page=int(parse_qs(parsed_ending_page_url.query)['page'][0])
+    return ending_page
+
 
 def scrape_urls():
-    base_url = "https://www.fs.usda.gov/visit/destinations?field_rec_activities_target_id=11905&field_rec_forest_target_id=All&page="
-    headers = {'User-Agent': 'Camping Tripper/0.0.1'}
-    starting_page = 0
-    ending_page = 224
-    output_file = 'camp_site_urls.csv'
-    sleep_time = 5
-
-    camp_sites = []
-
-    for i in range(starting_page, ending_page+1):
-        url = base_url + str(i)
+    camp_sites=[]
+    for page in range(STARTING_PAGE, ending_page+1):
+        url=BASE_URL + str(page)
         print(f'Sending GET request to {url}')
-        response = requests.get(url, headers=headers)
+        response=requests.get(url, headers=HEADERS)
         print(f'Status code: {response.status_code}')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        links = soup.find_all(attrs= {'class': 'usa-link'})
-
-        for link in links:
-            camp_sites_dc = {
-                'NAME': link.text,
-                'URL': link['href'],
+        soup=BeautifulSoup(response.content, 'html.parser')
+        urls=soup.find_all(attrs={'class': 'usa-link'})
+        for url in urls:
+            camp_sites_dc={
+                'NAME': url.text,
+                'URL': url['href'],
+                'PAGE_QUERY': page,
                 'DT': time.strftime('%m-%d-%Y %H:%M:%S')
                 }
             camp_sites.append(camp_sites_dc)
-        time.sleep(sleep_time)
+        time.sleep(SLEEP_TIME)
+    return camp_sites
 
-    df = pd.DataFrame(camp_sites) 
-    df.index.name = 'ID'
-    df.to_csv(output_file)
 
 if __name__ == "__main__":
-    scrape_urls()
+    BASE_URL="https://www.fs.usda.gov/visit/destinations?field_rec_activities_target_id=11905&field_rec_forest_target_id=All&page="
+    HEADERS={'User-Agent': 'Camping Tripper/0.0.1'}
+    OUTPUT_PATH="data/camp_site_urls/"
+    OUTPUT_FILE="camp_site_urls.csv"
+    STARTING_PAGE=0
+    SLEEP_TIME=5
+    first_page_url=BASE_URL+str(STARTING_PAGE)
+    ending_page=scrape_ending_page()
+    camp_sites=scrape_urls()
+    df=pd.DataFrame(camp_sites)
+    df.index.name='ID'
+    dt_str_path=create_date_str_path()
+    full_output_path=OUTPUT_PATH+dt_str_path
+    if not os.path.exists(full_output_path):
+        os.mkdir(full_output_path)
+    df.to_csv(full_output_path+OUTPUT_FILE)
