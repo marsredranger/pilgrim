5367511ff200eeb53a68de13da2b4c42db347819
redranges <redranges@protonmail.com>
2024-07-15 11:00:58 -0400

    Changes to camp_site_info.py to use new data directory
    will now read file from current day. New limitation is that you must run
    camp_site_urls.py on same day you run camp_site_info.py or you will get
    error

--- camp_site_info.py
+++ camp_site_info.py
@@ -6,53 +6,63 @@ from sys import argv
 import os
 
 
+def create_date_str_path():
+    return time.strftime("%m_%d_%Y/")
+
+
 def scrape_lat_long(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    divs = soup.find_all('div', attrs={'class': 'right-box'})
-    latitude = None
-    longitude = None
+    soup=BeautifulSoup(html, 'html.parser')
+    divs=soup.find_all('div', attrs={'class': 'right-box'})
+    latitude=None
+    longitude=None
     for div in divs:
         if 'Latitude' in div.text:
-            latitude = div.find_next_sibling('div').text.strip()
+            latitude=div.find_next_sibling('div').text.strip()
         if 'Longitude' in div.text:
-            longitude = div.find_next_sibling('div').text.strip()
+            longitude=div.find_next_sibling('div').text.strip()
         if latitude and longitude:
             break
     return (latitude, longitude)
 
 
 def fetch_camp_site_content(url):
-    headers = {'User-Agent': 'Camping Tripper/0.0.1'}
-    response = requests.get(url, headers=headers)
+    headers={'User-Agent': 'Pilgrim/0.0.1'}
+    response=requests.get(url, headers=headers)
     return response.content
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     try:
-        batch_start = int(argv[1])
-        batch_end = int(argv[2])
+        batch_start=int(argv[1])
+        batch_end=int(argv[2])
     except IndexError:
         raise SystemExit(f'Usage: {argv[0]} <batch_start> <batch_end>')
 
-    CAMP_SITE_CSV_PATH = './camp_site_urls.csv'
-    CAMP_SITE_INFO_CSV_PATH = './camp_site_info.csv'
-    SLEEP_TIME = 5
+    CAMP_SITE_URLS_PATH=f"data/camp_site_urls/{create_date_str_path()}"
+    CAMP_SITE_URLS_FILENAME='camp_site_urls.csv'
+    CAMP_SITE_INFO_PATH=f"data/camp_site_info/{create_date_str_path()}"
+    CAMP_SITE_INFO_FILENAME='camp_site_info.csv'
+    SLEEP_TIME=2
 
-    df = pd.read_csv(CAMP_SITE_CSV_PATH)
-    df = df.loc[batch_start:batch_end-1]
+    df=pd.read_csv(CAMP_SITE_URLS_PATH+CAMP_SITE_URLS_FILENAME)
+    df=df.loc[batch_start:batch_end-1]
 
     for i in range(batch_start, batch_end):
-        url = df.loc[i, 'URL']
+        url=df.loc[i, 'URL']
         print(f'Number in Batch {i+1}')
         print(f'Fetching camp site content from {url}')
-        html = fetch_camp_site_content(url)
-        latitude, longitude = scrape_lat_long(html)
-        df.loc[i, 'LATITUDE'] = latitude
-        df.loc[i, 'LONGITUDE'] = longitude
+        html=fetch_camp_site_content(url)
+        latitude, longitude=scrape_lat_long(html)
+        df.loc[i, 'LATITUDE']=latitude
+        df.loc[i, 'LONGITUDE']=longitude
         time.sleep(SLEEP_TIME)
 
-    write_header = False if os.path.exists(CAMP_SITE_INFO_CSV_PATH) else True
-    df.to_csv(CAMP_SITE_INFO_CSV_PATH,
+    write_header=False if os.path.exists(CAMP_SITE_INFO_PATH) else True
+
+    if write_header:
+        os.mkdir(CAMP_SITE_INFO_PATH)
+
+    df.to_csv(CAMP_SITE_INFO_PATH+CAMP_SITE_INFO_FILENAME,
               mode='a',
               index=False,
               header=write_header)
